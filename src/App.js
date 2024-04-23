5952ababb0c49526540a27f05dd1bbf69f2c8357
iay <septianadip7998@gmail.com>
2024-04-23 14:53:25 +0700

    whatsaap added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Footer from "./components/Footer";
 class App extends Component {
   render(){
     return (  
+      <>
       <div>
         <NavbarSection/>
         <Header/>
@@ -16,6 +17,12 @@ class App extends Component {
         <Partners/>
         <Footer/>
       </div>
+      <div className="fixed-bottom right-100 p-3" style={{zIndex: "6", left: "initial"}}>
+        <a href="https://wa.me/6282310001908?text=Hello, How can i help you?" target="_blank" rel="noreferrer">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/WhatsApp_icon.png/598px-WhatsApp_icon.png" alt="wa kami" width="60"/>
+        </a>
+      </div>
+      </>
     );  
   }  
 }  
--- src/components/Events.js
+++ src/components/Events.js
@@ -16,22 +16,38 @@ const Events = () => {
     },
     button: {
       backgroundColor: "#c9e34a",
+      fontWeight: "bold",
     },
     buttonHover: {
       backgroundColor: "#a0cf41",
     }
   };
 
+  const handleMouseEnter = (e) => {
+    e.target.style.backgroundColor = styles.buttonHover.backgroundColor;
+  };
+
+  const handleMouseLeave = (e) => {
+    e.target.style.backgroundColor = styles.button.backgroundColor;
+  };
+
   return (
     <div className="container">
       <h1 className="text-center p-2 m-4" style={styles.h1}>Upcoming Events</h1>
-      <div className="row">
+      <div className="container">
+      <div className="row align-content-center align-items-center d-flex justify-content-center">
         <div className="col-sm-6">
           <div className="card mb-3" style={styles.card}>
             <div className="card-body d-flex flex-column align-items-center">
               <img src="https://www.tbnindonesia.org/images/tbn-asia-conference-2023-2.jpg" alt="tbn-asia-conference" className="rounded" style={styles.img}/>
               <h5 className="card-title text-center">TBN Asia Conference 2023</h5>
-              <a href="https://www.tbnindonesia.org/images/tbn-asia-conference-2023-2.jpg" className="btn" style={styles.button}>
+              <a
+                href="https://www.tbnindonesia.org/images/tbn-asia-conference-2023-2.jpg"
+                className="btn"
+                style={styles.button}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+              >
                 Look up the conference
               </a>
             </div>
@@ -42,12 +58,14 @@ const Events = () => {
             <div className="card-body d-flex flex-column align-items-center">
               <img src="https://www.tbnindonesia.org/images/transformational-sales-conference-2023-.jpg" alt="transformational-sales-conference-2023" className="rounded" style={styles.img} />
               <h5 className="card-title text-center">Transformational Sales Conference 2023</h5>
-              <a href="https://www.tbnindonesia.org/images/transformational-sales-conference-2023-.jpg" className="btn" style={styles.button}>
+              <a href="https://www.tbnindonesia.org/images/transformational-sales-conference-2023-.jpg" className="btn" style={styles.button} onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}>
               Look up the conference
               </a>
             </div>
           </div>
         </div>
+      </div>
       </div>
     </div>
   );
